07f8015c9689924857b11870fb4b1bc925ee2f23
Brandon Lee <brandonlee2543@hotmail.com>
2019-05-01 16:40:08 -0400

    Link calculator.js to index.html

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -12,6 +12,41 @@ function calculatorDisplayUpdate() {
 
 calculatorDisplayUpdate();
 
+const keypresses = document.querySelector('.calculator-keys');
+keypresses.addEventListener('click', event => {
+  const { target } = event;
+  if (!target.matches('input')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('Operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('Decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('clear')) {
+    console.log('Clear', target.value);
+    return;
+  }
+
+  if (target.classList.contains('clear-entry')) {
+    console.log('Clear entry', target.value);
+    return;
+  }
+
+  if (target.classList.contains('equals')) {
+    console.log('Equals', target.value);
+    return;
+  }
+
+  console.log('Digit', target.value);
+});
+
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
@@ -20,5 +55,3 @@ function multiply(array) {
     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
     : 0;
 }
-
-function operate(operator, num1, num2) {}
